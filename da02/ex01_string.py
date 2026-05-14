0b72dc2a83d1d0076344992dccbeea3e7866bdfd
Victor Jung <jungwonil11@gmail.com>
2026-05-14 14:23:32 +0900

    wip: 싱크 전 저장

--- da02/ex01_string.py
+++
@@ -1,9 +0,0 @@
-str = "Hello"
-# in : 포함 되면 True
-print("H" in str)     # True
-print("llo" in str)   # True
-print("hello" in str) # False
-
-# not in : 포함되지 않으면 True
-print("x" not in str)      # True
-print("Hello" not in str)  # False
